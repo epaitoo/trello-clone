ed305b2f2c10d501821759af74525071be34e100
Eugene Paitoo <epaitoo@toldoit.com>
2020-12-22 20:40:48 +0000

    automatically cursor on input using ref

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useFocus } from "../utils/useFocus";
 // import useState from "react";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "../styles";
 
@@ -8,12 +9,21 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
+
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text)
+      }      
+  }
 
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={(e: any) => setText(e.target.value)}
+        onKeyPress={handleAddText}
       />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
